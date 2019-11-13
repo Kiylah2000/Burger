9009db5e36bf6bde7daeeaca222e43c203d98324
Kiylah O'Brien <kiylah2000@gmail.com>
2019-11-12 21:56:38 -0500

    created a directory for the burger app

---
+++ config/orm.js
@@ -0,0 +1,8 @@
+// Import MySQL connection.
+var connection = require("./connection.js.js");
+
+
+
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO burger (name, sleepy) VALUES ('Baconator', true);
+INSERT INTO burger (name, sleepy) VALUES ('Double Cheese Burger', False);
+INSERT INTO burger (name, sleepy) VALUES ('Fish Burger', true);
+
+
+
