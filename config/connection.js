621b4bb69eb0da6c503939a857efc0766ac70611
Kiylah O'Brien <kiylah2000@gmail.com>
2020-01-12 12:53:31 -0500

    updated connection code

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-
-var connection;
 if (process.env.JAWSDB_URL) {
     // Database is JawsDB on Heroku
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     // Database is local
-    connection = mysql.createConnection({
+    var connection = mysql.createConnection({
         port: 3306,
         host: 'localhost',
         user: 'root',
         password: "princessK95$",
         database: "burger_db"
-    })
-};
+    });
+}
 app.listen(process.env.PORT || 8080);
 
-var mysql = require('mysql');
 // Make connection.
 connection.connect(function(err) {
   if (err) {
